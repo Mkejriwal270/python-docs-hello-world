76b16aa3b9f95ba6b40f98ce97146deb5922c6c7
Mkejriwal270 <mkejriwal270@gmail.com>
2020-08-09 01:29:06 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World Azure Pipelines Build!"
+    return "Hello World Azure Pipelines Build! This will go to both pipelines and then I'll delete the deploy part"
